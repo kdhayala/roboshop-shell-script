02ca4cebf69f05749765c16a57efc4dff8bd4ade
kumar <kumardhayait15@gmail.com>
2022-12-21 23:07:59 +0530

    Mongodb installation

--- roboshop.sh
+++ roboshop.sh
@@ -2,21 +2,21 @@ LOG_FILE=/tmp/frontend
 
 echo Installing Nginx
 yum install nginx -y &>>$LOG_FILE
-StatusCheck $?
+StatusCheck = $?
 
 echo Downloading Nginx Web Content
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
-StatusCheck $?
+StatusCheck = $?
 
 cd /usr/share/nginx/html
 
 echo Removing Old Web Content
 rm -rf * &>>/tmp/frontend
-StatusCheck $?
+StatusCheck = $?
 
 echo Extracting Web Content
 unzip /tmp/frontend.zip &>>$LOG_FILE
-StatusCheck $?
+StatusCheck = $?
 
 mv frontend-main/static/* . &>>$LOG_FILE
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
@@ -24,4 +24,4 @@ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 echo Starting Nginx Service
 systemctl enable nginx &>>$LOG_FILE
 systemctl restart nginx &>>$LOG_FILE
-StatusCheck $?
+StatusCheck = $?
