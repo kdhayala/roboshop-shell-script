b030ae0225be69941c60d8309403ebd3c2fc7f53
kumar <kumardhayait15@gmail.com>
2022-12-21 22:59:34 +0530

    frontend.sh

--- roboshop.sh
+++ roboshop.sh
@@ -1,25 +1,27 @@
-echo  installing Nginx
-yum install nginx -y &>>/tmp/frontend
-echo Status = $?
+LOG_FILE=/tmp/frontend
 
-echo Downloading Nginx Web content
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
-echo Status = $?
+echo Installing Nginx
+yum install nginx -y &>>$LOG_FILE
+StatusCheck $?
+
+echo Downloading Nginx Web Content
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+StatusCheck $?
 
 cd /usr/share/nginx/html
 
-echo removing the old web content
+echo Removing Old Web Content
 rm -rf * &>>/tmp/frontend
-echo Status = $?
+StatusCheck $?
 
-echo extracting the web content
-unzip /tmp/frontend.zip &>>/tmp/frontend
-echo Status = $?
+echo Extracting Web Content
+unzip /tmp/frontend.zip &>>$LOG_FILE
+StatusCheck $?
 
-mv frontend-main/static/* . &>>/tmp/frontend
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
+mv frontend-main/static/* . &>>$LOG_FILE
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 
-echo Starting Nginx service
-systemctl enable nginx &>>/tmp/frontend
-systemctl restart nginx &>>/tmp/frontend
-echo Status = $?
+echo Starting Nginx Service
+systemctl enable nginx &>>$LOG_FILE
+systemctl restart nginx &>>$LOG_FILE
+StatusCheck $?
