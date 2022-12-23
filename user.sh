154caea7ed8866b647fcbc8eb3d6a1f8f30e5eb5
kumar <kumardhayait15@gmail.com>
2022-12-23 22:36:33 +0530

    ll u

--- user.sh
+++ user.sh
@@ -1,20 +1,23 @@
-LOG_FILE=/tmp/cataloguer
+LOG_FILE=/tmp/user
 
 source common.sh
 
 COMPONENT=user
 
 
-  echo "Update systemD service file"
-  sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' 's/MONGO_ENDPOINT/redis.roboshop.internal/'  /home/roboshop/user/systemd.service
-  StatusCheck $?
+echo "Update systemD service file"
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' 's/MONGO_ENDPOINT/redis.roboshop.internal/'  /home/roboshop/user/systemd.service
+StatusCheck $?
 
-  mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service &>>${LOG_FILE}
-  systemctl daemon-reload  &>>${LOG_FILE}
-  systemctl enable user &>>${LOG_FILE}
-  systemctl start user &>>${LOG_FILE}
-  StatusCheck $
-  }
+echo  "Setup User service"
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service &>>${LOG_FILE}
+StatusCheck $?
+
+systemctl daemon-reload  &>>${LOG_FILE}
+systemctl enable user &>>${LOG_FILE}
+systemctl start user &>>${LOG_FILE}
+StatusCheck $
+
 
 
 
