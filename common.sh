fe3320e33153aeb1b269195d4f88733b452902a7
kumar <kumardhayait15@gmail.com>
2022-12-23 22:31:49 +0530

    updating color code correctionll

--- common.sh
+++ common.sh
@@ -1,14 +1,14 @@
 ID=$(id -u)
+ID=$(id -u)
 if [ $ID -ne 0 ]; then
-  echo You should run the script as a root user.
+  echo You should run this script as root user or with sudo privileges.
   exit 1
 fi
-
 StatusCheck() {
   if [ $1 -eq 0 ]; then
-    echo -e  Status = "\e[32mSuccess\e[0m"
+    echo -e Status = "\e[32mSUCCESS\e[0m"
   else
-    echo -e Status = "\e[31mFailure\e[0m"
+    echo -e Status = "\e[31mFAILURE\e[0m"
     exit 1
   fi
 }
